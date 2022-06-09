7d15b31257d140eab540ff99f1f52d3d61721b18
federicovicentini <federico.vicentini99@gmail.com>
2022-06-09 12:36:01 +0200

    Update Exercise 2.r

--- Exercise 2.r
+++ Exercise 2.r
@@ -16,7 +16,9 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # Install packages
 packages <- c("tidyverse", "rsdmx", "eurostat", "tbl2xts", 
               "tidyquant", "BCDating", "pwt10", "dplyr",
-              "stargazer", "car", "forecast", "tseries", "quantmod", "eurostat")
+              "stargazer", "car", "forecast", "tseries", 
+              "quantmod", "eurostat", "stargazer",
+              "skedastic","Metrics","mFilter", "aTSA","lmtest","xts")
 new.packages <- packages[!(packages %in% installed.packages()[, "Package"])]
 if (length(new.packages)) install.packages(new.packages)
 invisible(lapply(packages, library, character.only = TRUE))
@@ -39,18 +41,169 @@ library(eurostat)
 i <- 1
 j <- 2
 
-# generate a random series
-y <- rnorm(100)
+set.seed(1111)
 
-# compute AR(i) of y
-ar.i <- ar(y, i)
+# Define a function to generate a general ARMA time series of order i,j
+# so that it could be also an AR or an MA
 
-# prediction of AR(i)
-pred.ar.i <- ts(predict(ar.i, h = 12))
+arma=function(ar,ma){
+  armats <- arima.sim(model=list(ar=ar,ma=ma), n=1000)
+  armats
+  return(armats)
+}
+
+armafit=function(armats,order){
+  armatsfit = arima(armats, order)
+  return(armatsfit)
+}
+
+plotcorr=function(ts,main){
+  plot(ts, type="l", main=main)
+  pacf(ts, main=main)
+}
+
+#Simulate an AR(1) with certain parameters
+
+ar=c(0.3)
+ma=c(0)
+order=c(1,0,0)
+
+
+arts1=arma(ar,ma)
+artsfit1 = armafit(arts1, order)
+stargazer(artsfit1, type="text")
+
+plotcorr(arts1,main="AR(1)")
+
+#Simulate an AR(1) with certain parameters
+
+ar=c(0.5)
+ma=c(0)
+order=c(1,0,0)
+
+
+arts1=arma(ar,ma)
+artsfit1 = armafit(arts1, order)
+stargazer(artsfit1, type="text")
+
+plotcorr(arts1,main="AR(1)")
+
+#Simulate an AR(1) with certain parameters
+
+ar=c(-0.3)
+ma=c(0)
+order=c(1,0,0)
+
+
+arts1=arma(ar,ma)
+artsfit1 = armafit(arts1, order)
+stargazer(artsfit1, type="text")
+
+plotcorr(arts1,main="AR(1)")
+
+#Simulate an AR(1) with certain parameters
+
+ar=c(-0.5)
+ma=c(0)
+order=c(1,0,0)
+
+
+arts1=arma(ar,ma)
+artsfit1 = armafit(arts1, order)
+stargazer(artsfit1, type="text")
+
+plotcorr(arts1,main="AR(1)")
+
+
+#Simulate an AR(4) with certain parameters
+
+ar=c(0.3,0.25,-0.22,-0.15)
+ma=c(0)
+order=c(4,0,0)
+
+
+arts1=arma(ar,ma)
+artsfit1 = armafit(arts1, order)
+stargazer(artsfit1, type="text")
+
+plotcorr(arts1,main="AR(4)")
+
+# Simulate an AR(6) with certain parameters
+
+ar=c(0.35,0.33,0.3,-0.18, -0.15, -0.1)
+ma=c(0)
+order=c(6,0,0)
+
+
+arts2=arma(ar,ma)
+artsfit2 = armafit(arts2, order)
+stargazer(artsfit2, type="text")
+
+plotcorr(arts2,main="AR(6)")
+
+
+# Simulate an MA(4) with certain parameters
+
+ar=c(0)
+ma=c(0.2,0.2,-0.2,-0.2)
+order=c(0,0,4)
+
+
+mats1=arma(ar,ma)
+matsfit1 = armafit(mats1, order)
+stargazer(matsfit1, type="text")
+
+plotcorr(mats1,main="MA(4)")
+
+# Simulate an MA(6) with certain parameters
+
+ar=c(0)
+ma=c(0.2,0.2,0.2,-0.2,-0.2,-0.2)
+order=c(0,0,6)
+
+
+mats2=arma(ar,ma)
+matsfit2 = armafit(mats2, order)
+stargazer(matsfit2, type="text")
+
+plotcorr(mats2,main="MA(6)")
+
+# Simulate an ARMA(3,1) with certain parameters
+
+ar=c(0.2,0.2,-0.2)
+ma=c(-0.2)
+order=c(3,0,1)
+
+
+armats1=arma(ar,ma)
+armatsfit1 = armafit(armats1, order)
+stargazer(armatsfit1, type="text")
+
+plotcorr(armats1,main="ARMA(3,1)")
+
+# Simulate an ARMA(1,3) with certain parameters
+
+ar=c(0.1)
+ma=c(0.1,-0.1,-0.1)
+order=c(1,0,3)
+
+
+armats2=arma(ar,ma)
+armatsfit2 = armafit(armats2, order)
+stargazer(armatsfit2, type="text")
+
+plotcorr(armats2,main="ARMA(1,3)")
 
 # plot the prediction of AR(i) and the original data
 plot(y, type = "l", col = "blue", lwd = 2, xlab = "Time", ylab = "Log PCEC")
 lines(pred.ar.i, type = "l", col = "red", lwd = 2, lty = 2)
+
+
+
+
+
+
+
 
 # compute MA(i) of y
 ma.i <- ma(y, i)
@@ -85,6 +238,9 @@ plot(acf.ar.i)
 plot(acf.ma.i)
 # plot the autocorrelation function of the ARMA(i,j)
 plot(acf.arma.ij)
+
+
+
 
 
 
