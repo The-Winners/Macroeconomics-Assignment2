4dd4f4296ae651e0fab71bbcfc1c9c2ef51c8e90
federicovicentini <federico.vicentini99@gmail.com>
2022-06-09 10:18:54 +0200

    Update Exercise 2.r

--- Exercise 2.r
+++ Exercise 2.r
@@ -5,6 +5,31 @@
 library(forecast)
 library(tseries)
 
+# PRELIMINARY OPERATIONS
+
+
+# Clear the variables
+rm(list = ls())
+
+# Set the working directory to source file location with
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Install packages
+packages <- c("tidyverse", "rsdmx", "eurostat", "tbl2xts", 
+              "tidyquant", "BCDating", "pwt10", "dplyr",
+              "stargazer", "car", "forecast", "tseries", "quantmod","eurostat")
+new.packages <- packages[!(packages %in% installed.packages()[, "Package"])]
+if (length(new.packages)) install.packages(new.packages)
+invisible(lapply(packages, library, character.only = TRUE))
+
+# Load packages
+library(quantmod)
+library(eurostat)
+
+##################
+##### POINT 1 ####
+##################
+
 
 # 1 - Data generating process
 # Simulate an AR(i), a MA(i) and an ARMA(i,j) study the correlogram,
@@ -60,3 +85,10 @@ plot(acf.ar.i)
 plot(acf.ma.i)
 # plot the autocorrelation function of the ARMA(i,j)
 plot(acf.arma.ij)
+
+
+
+##################
+##### POINT 2 ####
+##################
+
