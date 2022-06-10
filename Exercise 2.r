9912f2ac109ac02899ec3c904973c0f6ac9ad709
federicovicentini <federico.vicentini99@gmail.com>
2022-06-10 11:08:26 +0200

    Update Exercise 2.r

--- Exercise 2.r
+++ Exercise 2.r
@@ -59,6 +59,7 @@ armafit=function(armats,order){
 
 plotcorr=function(ts,main){
   plot(ts, type="l", main=main)
+  acf(ts, main=main)
   pacf(ts, main=main)
 }
 
@@ -75,7 +76,7 @@ arts1=arma(ar,ma)
 artsfit1 = armafit(arts1, order)
 stargazer(artsfit1, type="text")
 
-plotcorr(arts1,main="AR(1)")
+plotcorr(arts1,main="AR(1)pos.a")
 
 #Simulate an AR(1) with a positive parameter
 
@@ -88,7 +89,7 @@ arts2=arma(ar,ma)
 artsfit2 = armafit(arts2, order)
 stargazer(artsfit2, type="text")
 
-plotcorr(arts2,main="AR(1)")
+plotcorr(arts2,main="AR(1)pos.b")
 
 #Simulate an AR(1) with a negative parameter
 
@@ -101,7 +102,7 @@ arts3=arma(ar,ma)
 artsfit3 = armafit(arts3, order)
 stargazer(artsfit3, type="text")
 
-plotcorr(arts3,main="AR(1)")
+plotcorr(arts3,main="AR(1)neg.a")
 
 #Simulate an AR(1) with a negative parameter
 
@@ -114,7 +115,7 @@ arts4=arma(ar,ma)
 artsfit4 = armafit(arts4, order)
 stargazer(artsfit4, type="text")
 
-plotcorr(arts4,main="AR(1)")
+plotcorr(arts4,main="AR(1)neg.b")
 
 
 #Simulate an MA(1) with a positive parameter
@@ -124,19 +125,86 @@ ma=c(0.2)
 order=c(0,0,1)
 
 
-arts1=arma(ar,ma)
-artsfit1 = armafit(arts1, order)
-stargazer(artsfit1, type="text")
+mats1=arma(ar,ma)
+matsfit1 = armafit(mats1, order)
+stargazer(matsfit1, type="text")
 
-plotcorr(arts1,main="AR(1)")
-
-
+plotcorr(mats1,main="MA(1)pos.a")
 
 
+#Simulate an MA(1) with a positive parameter
+
+ar=c(0)
+ma=c(0.3)
+order=c(0,0,1)
+
+
+mats2=arma(ar,ma)
+matsfit2 = armafit(mats2, order)
+stargazer(matsfit2, type="text")
+
+plotcorr(mats2,main="MA(1)pos.b")
+
+
+#Simulate an MA(1) with a negative parameter
+
+ar=c(0)
+ma=c(-0.2)
+order=c(0,0,1)
+
+
+mats3=arma(ar,ma)
+matsfit3 = armafit(mats3, order)
+stargazer(matsfit3, type="text")
+
+plotcorr(mats3,main="MA(1)neg.a")
 
 
 
+#Simulate an MA(1) with a negative parameter
 
+ar=c(0)
+ma=c(-0.3)
+order=c(0,0,1)
+
+
+mats4=arma(ar,ma)
+matsfit4 = armafit(mats4, order)
+stargazer(matsfit4, type="text")
+
+plotcorr(mats4,main="MA(1)neg.b")
+
+
+
+# Simulate an ARMA(2,2) with certain parameters
+
+ar=c(0.5,-0.3)
+ma=c(0.2,-0.2)
+order=c(2,0,2)
+
+
+armats1=arma(ar,ma)
+armatsfit1 = armafit(armats1, order)
+stargazer(armatsfit1, type="text")
+
+plotcorr(armats1,main="ARMA(2,2)")
+
+
+# Simulate an ARMA(2,2) with certain parameters
+
+ar=c(0.3,-0.5)
+ma=c(-0.2, 0.2)
+order=c(2,0,2)
+
+
+armats2=arma(ar,ma)
+armatsfit2 = armafit(armats2, order)
+stargazer(armatsfit2, type="text")
+
+plotcorr(armats2,main="ARMA(2,2)")
+
+
+# INTERPRETATION 2
 
 #Simulate an AR(4) with certain parameters
 
@@ -217,50 +285,6 @@ stargazer(armatsfit2, type="text")
 
 plotcorr(armats2,main="ARMA(1,3)")
 
-# plot the prediction of AR(i) and the original data
-plot(y, type = "l", col = "blue", lwd = 2, xlab = "Time", ylab = "Log PCEC")
-lines(pred.ar.i, type = "l", col = "red", lwd = 2, lty = 2)
-
-
-
-
-
-
-
-
-# compute MA(i) of y
-ma.i <- ma(y, i)
-
-# compute ARMA(i,j) of y
-arma.ij <- arma(x = y, order = c(i, j))
-
-
-# Plot the correlogram of the three models
-
-# plot the AR(i)
-plot(ar.i)
-
-# plot the MA(i)
-plot(ma.i)
-
-# plot the ARMA(i,j)
-plot(arma.ij)
-
-# 3 - Compute the autocorrelation function of the three models
-# compute the autocorrelation function of the AR(i)
-acf.ar.i <- acf(ar.i)
-# compute the autocorrelation function of the MA(i)
-acf.ma.i <- acf(ma.i)
-# compute the autocorrelation function of the ARMA(i,j)
-acf.arma.ij <- acf(arma.ij)
-
-# 4 - Plot the autocorrelation function of the three models
-# plot the autocorrelation function of the AR(i)
-plot(acf.ar.i)
-# plot the autocorrelation function of the MA(i)
-plot(acf.ma.i)
-# plot the autocorrelation function of the ARMA(i,j)
-plot(acf.arma.ij)
 
 
 
